14979b5799f5e0fca570c6e2f054a8e1f1a2bc7a
Emilia Jaser <vapor.schitcrafter@gmail.com>
2025-04-29 15:45:36 +0200

    ✨ vscode-remote-server setup

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, vscode-server, ... }@inputs: {
     nixosConfigurations.nixnest = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -24,6 +25,10 @@
 
             home-manager.users.emi = import ./home.nix;
           }
+        vscode-server.nixosModules.default
+        ({ config, pkgs, ... }: {
+          services.vscode-server.enable = true;
+        })
       ];
     };
   };
--- home.nix
+++ home.nix
@@ -1,30 +1,30 @@
 { config, pkgs, ... }:
 
 {
-    home.username = "emi";
-    home.homeDirectory = "/home/emi";
+  home.username = "emi";
+  home.homeDirectory = "/home/emi";
 
-    programs.git = {
-        enable = true;
-        userName = "Emilia Jaser";
-        userEmail = "vapor.schitcrafter@gmail.com";
+  programs.git = {
+    enable = true;
+    userName = "Emilia Jaser";
+    userEmail = "vapor.schitcrafter@gmail.com";
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+        update = "nixos-rebuild switch --use-remote-sudo";
     };
+  };
 
-    programs.starship = {
-        enable = true;
-    };
-
-    programs.zsh = {
-        enable = true;
-        autocd = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
-        shellAliases = {
-            update = "nixos-rebuild switch --use-remote-sudo";
-        };
-    };
-
-    home.stateVersion = "24.11";
-    programs.home-manager.enable = true;
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
 }
