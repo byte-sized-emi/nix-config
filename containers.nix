433c31d77d0fc4f500306fa5a20292a1891f384e
Emilia Jaser <vapor.schitcrafter@gmail.com>
2025-05-12 00:10:32 +0200

    ✨ Home assistant voice setup

--- containers.nix
+++ containers.nix
@@ -28,27 +28,40 @@ in {
   virtualisation.quadlet.containers.home-assistant = {
     containerConfig = {
       image = "ghcr.io/home-assistant/home-assistant:2025.4.4";
-      environments = {
-        TZ = "Europe/Berlin";
-      };
-      exposePorts = [
-        "8123"
-      ];
-      addCapabilities = [
-        "CAP_NET_RAW"
-      ];
+      environments.TZ = "Europe/Berlin";
+      exposePorts = [ "8123" ];
+      addCapabilities = [ "CAP_NET_RAW" ];
       volumes = [
         "${homeAssistantPath}/config:/config"
         "${homeAssistantPath}/config/configuration.yaml:/config/configuration.yaml:ro"
         "/run/dbus:/run/dbus:ro"
         "/etc/localtime:/etc/localtime:ro"
       ];
-      networks = [
-        "host"
-      ];
-      labels = [
-        "wud.tag.include=^\d+\.\d+\.\d+$"
-      ];
+      networks = [ "host" ];
+    };
+    serviceConfig = {
+      Restart = "always";
+    };
+  };
+
+  virtualisation.quadlet.containers.whisper = {
+    containerConfig = {
+      image = "rhasspy/wyoming-whisper:2.4.0";
+      environments.TZ = "Europe/Berlin";
+      publishPorts = [ "127.0.0.1:10300:10300" ];
+      exec = "--model distil-small.en --language en";
+    };
+    serviceConfig = {
+      Restart = "always";
+    };
+  };
+
+  virtualisation.quadlet.containers.piper = {
+    containerConfig = {
+      image = "rhasspy/wyoming-piper:1.5.0";
+      environments.TZ = "Europe/Berlin";
+      publishPorts = [ "127.0.0.1:10200:10200" ];
+      exec = "--voice en_US-amy-medium";
     };
     serviceConfig = {
       Restart = "always";
