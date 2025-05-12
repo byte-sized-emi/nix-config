f79143d694dac3b6b0e6f4c886e312813a912883
Emilia Jaser <vapor.schitcrafter@gmail.com>
2025-05-12 22:21:17 +0200

    🎨 Improve container config

--- containers.nix
+++ containers.nix
@@ -1,4 +1,4 @@
-{...}: let
+{config, ...}: let
   homeAssistantPath = "/etc/stacks/home-assistant";
 in {
   virtualisation.containers.enable = true;
@@ -25,63 +25,71 @@ in {
     scene: !include scenes.yaml
   '';
 
-  virtualisation.quadlet.containers.home-assistant = {
-    containerConfig = {
-      image = "ghcr.io/home-assistant/home-assistant:2025.4.4";
-      environments.TZ = "Europe/Berlin";
-      exposePorts = [ "8123" ];
-      addCapabilities = [ "CAP_NET_RAW" ];
-      volumes = [
-        "${homeAssistantPath}/config:/config"
-        "${homeAssistantPath}/config/configuration.yaml:/config/configuration.yaml:ro"
-        "/run/dbus:/run/dbus:ro"
-        "/etc/localtime:/etc/localtime:ro"
-      ];
-      networks = [ "host" ];
+  virtualisation.quadlet = let
+    inherit (config.virtualisation.quadlet) volumes;
+  in {
+    containers.home-assistant = {
+      containerConfig = {
+        image = "ghcr.io/home-assistant/home-assistant:2025.4.4";
+        environments.TZ = "Europe/Berlin";
+        exposePorts = [ "8123" ];
+        addCapabilities = [ "CAP_NET_RAW" ];
+        volumes = [
+          "${homeAssistantPath}/config:/config"
+          "${homeAssistantPath}/config/configuration.yaml:/config/configuration.yaml:ro"
+          "/run/dbus:/run/dbus:ro"
+          "/etc/localtime:/etc/localtime:ro"
+        ];
+        networks = [ "host" ];
+      };
+      serviceConfig = {
+        Restart = "always";
+      };
     };
-    serviceConfig = {
-      Restart = "always";
-    };
-  };
 
-  virtualisation.quadlet.volumes.whisper-data = {
-    volumeConfig = {
-      type = "bind";
-      device = "/tmp/whisper-data";
+    volumes.whisper-data = {
+      volumeConfig = {
+        type = "bind";
+        device = "/tmp/whisper-data";
+      };
     };
-  };
 
-  virtualisation.quadlet.volumes.piper-data = {
-    volumeConfig = {
-      type = "bind";
-      device = "/tmp/piper-data";
+    volumes.piper-data = {
+      volumeConfig = {
+        type = "bind";
+        device = "/tmp/piper-data";
+      };
     };
-  };
 
-  virtualisation.quadlet.containers.whisper = {
-    # volume
-    containerConfig = {
-      image = "rhasspy/wyoming-whisper:2.4.0";
-      environments.TZ = "Europe/Berlin";
-      publishPorts = [ "127.0.0.1:10300:10300" ];
-      exec = "--model small-int8 --language de";
-      volumes = [ "whisper-data:/data" ];
+    containers.whisper = {
+      # volume
+      containerConfig = {
+        image = "rhasspy/wyoming-whisper:2.4.0";
+        environments.TZ = "Europe/Berlin";
+        publishPorts = [ "127.0.0.1:10300:10300" ];
+        exec = "--model small-int8 --language de";
+        volumes = [
+          "${volumes.whisper-data.ref}:/data"
+        ];
+      };
+      serviceConfig = {
+        Restart = "always";
+      };
     };
-    serviceConfig = {
-      Restart = "always";
-    };
-  };
 
-  virtualisation.quadlet.containers.piper = {
-    containerConfig = {
-      image = "rhasspy/wyoming-piper:1.5.0";
-      environments.TZ = "Europe/Berlin";
-      publishPorts = [ "127.0.0.1:10200:10200" ];
-      exec = "--voice de_DE-ramona-low";
-      volumes = [ "piper-data:/data" ];
-    };
-    serviceConfig = {
-      Restart = "always";
+    containers.piper = {
+      containerConfig = {
+        image = "rhasspy/wyoming-piper:1.5.0";
+        environments.TZ = "Europe/Berlin";
+        publishPorts = [ "127.0.0.1:10200:10200" ];
+        exec = "--voice de_DE-ramona-low";
+        volumes = [
+          "${volumes.piper-data.ref}:/data"
+        ];
+      };
+      serviceConfig = {
+        Restart = "always";
+      };
     };
   };
 }
