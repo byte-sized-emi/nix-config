2d816d20b4e0d02a8c89714e91b3b6fbd99a2527
Emilia Jaser <vapor.schitcrafter@gmail.com>
2026-01-23 19:24:15 +0100

    Add forgejo update workflow + bind systemd socket to tailscale

--- modules/auto-update.nix
+++ modules/auto-update.nix
@@ -1,16 +1,5 @@
 { pkgs, lib, ... }:
-let
-  user = "root";
-in
 {
-  # users.users.${user} = {
-  #   isSystemUser = true;
-  #   description = "User to run the nix update server";
-  #   createHome = false;
-  #   group = user;
-  # };
-  # users.groups.${user} = { };
-
   environment.systemPackages = [ pkgs.nix-update-server ];
   systemd.services.update-daemon = {
     description = "Socket activated daemon to update the NixOS system based on a git branch";
@@ -27,34 +16,11 @@ in
   systemd.sockets.update-daemon = {
     wantedBy = [ "sockets.target" ];
     socketConfig = {
-      ListenStream = "127.0.0.1:36196";
+      ListenStream = 36196;
       Accept = false;
-      # BindToDevice = "tailscale0";
+      BindToDevice = "tailscale0";
     };
   };
-
-  # security.doas.extraRules = [
-  #   {
-  #     cmd = "nix-env";
-  #     args = [ "switch" ];
-  #     noPass = true;
-  #     users = [ user ];
-  #   }
-  # ];
-
-  # security.sudo.extraRules = [
-  #   {
-  #     users = [ user ];
-  #     commands = [
-  #       {
-  #         command = "/run/current-system/sw/bin/nix-env";
-  #         options = [
-  #           "NOPASSWD"
-  #         ];
-  #       }
-  #     ];
-  #   }
-  # ];
 
   programs.git.enable = true;
   programs.git.config.safe.directory = "/home/emilia/nix-config";
