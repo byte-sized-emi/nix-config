90beabaf6325776c70a898fd8b32d4f52d407aa1
Emilia Jaser <vapor.schitcrafter@gmail.com>
2026-02-20 14:35:33 +0100

    fix directive

--- nix/hosts/nixnest/linktree.nix
+++ nix/hosts/nixnest/linktree.nix
@@ -2,9 +2,7 @@
 {
   services.caddy.virtualHosts."linktree.${config.settings.services.domain}".extraConfig = ''
     encode
-    files {
-      root * ${perSystem.self.linktree}
-      file_server
-    }
+    root * ${perSystem.self.linktree}
+    file_server
   '';
 }
