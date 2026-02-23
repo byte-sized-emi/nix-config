9c7ef432845e4146c96c0b838de433eb0a2723bb
Emilia Jaser <vapor.schitcrafter@gmail.com>
2026-02-23 20:34:41 +0100

    Add .etag files to linktree, to make etag caching work

--- nix/hosts/nixnest/linktree.nix
+++ nix/hosts/nixnest/linktree.nix
@@ -16,12 +16,14 @@ in
   };
 
   services.caddy.virtualHosts."${config.settings.linktree.domain}".extraConfig = ''
-        header {
-    		  -Last-Modified
-    		}
-        tls ${cert} ${key}
-        encode
-        root * ${perSystem.self.linktree}
-        file_server
+    header {
+      -Last-Modified
+    }
+    tls ${cert} ${key}
+    encode
+    root * ${perSystem.self.linktree}
+    file_server {
+      etag_file_extensions .etag
+    }
   '';
 }
--- nix/packages/linktree/default.nix
+++ nix/packages/linktree/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 pkgs.stdenvNoCC.mkDerivation {
   pname = "linktree";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = ./.;
 
@@ -10,6 +10,10 @@ pkgs.stdenvNoCC.mkDerivation {
     cp *.html $out/
     cp *.css $out/
     cp *.webp $out/
+    find "$out" -type f ! -name '*.etag' -print0 | while IFS= read -r -d "" file; do
+      # compute md5 hash and write only the hash to a .etag file next to the original file
+      md5sum "$file" | awk '{print $1}' > "$file.etag"
+    done
   '';
 
   meta = with pkgs.lib; {
