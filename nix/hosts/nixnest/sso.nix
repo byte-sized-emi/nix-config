985bd88967a5c176af0a905370fd13dc3a3c5c73
byte-sized-emi <emilia.git@byte-sized.fyi>
2026-03-12 20:41:54 +0100

    add testemi sso user

--- nix/hosts/nixnest/sso.nix
+++ nix/hosts/nixnest/sso.nix
@@ -91,6 +91,14 @@ in
           ];
           groups = [ "git" ];
         };
+        testemi = {
+          displayName = "Test Emi";
+          mailAddresses = [
+            "testemi@sso.byte-sized.fyi"
+            "testemi@byte-sized.fyi"
+          ];
+          groups = [ "git" ];
+        };
       };
       systems.oauth2 = {
         tailscale = {
