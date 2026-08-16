6f00d3830a5bc5358718541a9ce7c9ed59ba170b
byte-sized-emi <emilia.git@byte-sized.fyi>
2026-08-16 17:06:26 +0200

    :wrench: update freshrss config

--- modules/stacks/freshrss.nix
+++ modules/stacks/freshrss.nix
@@ -1,6 +1,6 @@
 { lib, ... }:
 {
-  stacks.freshrss.nixos = { config, ... }: {
+  stacks.freshrss.nixos = { config, pkgs, ... }: {
     sops.secrets."freshrss/password" = {
       owner = "freshrss";
       group = "freshrss";
@@ -12,7 +12,11 @@
       baseUrl = "https://freshrss.${config.settings.services.domain}";
       webserver = "caddy";
       defaultUser = "emilia";
+      api.enable = true;
       passwordFile = config.sops.secrets."freshrss/password".path;
+      extensions = with pkgs.freshrss-extensions; [
+        reading-time
+      ];
     };
 
     services.caddy.virtualHosts.${config.services.freshrss.virtualHost}.extraConfig = lib.mkBefore ''
